689ce37a1bc69a1cb385e7d527f4158752fc2191
Samrudhp <samrudhprakash3084@gmail.com>
2024-12-21 17:23:59 +0530

    user route and middleware

--- server/server.js
+++ server/server.js
@@ -1,27 +1,29 @@
-const express = require("express");
-const app = express();
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
 
-require("dotenv").config();
-const PORT = process.env.PORT || 5000;
+const authRoutes = require('./routes/auth');
+const applicationRoutes = require('./routes/application');
+const userRoutes = require('./routes/users');
 
-//middlewares
+const app = express();
+
+//middleware
 app.use(cors());
 app.use(express.json());
 
-//mounting
 app.use('/api/auth', authRoutes);
 app.use('/api/applications', applicationRoutes);
 app.use('/api/users', userRoutes);
 
 require("./config/db").connectDB();
 
-
-app.listen(PORT,()=>{
-    console.log(`server running on port ${PORT}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
-
-
-
-
+app.get("/",(req,res)=>{
+    res.send("<h1>Default page</h1>");
+});
