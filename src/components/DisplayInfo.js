80a7ee033e303ee7a7ba62c57c36bdc836996e5f
Samrudhp <samrudhprakash3084@gmail.com>
2025-03-01 13:58:42 +0530

    -

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -10,7 +10,7 @@ const DisplayInfo = () => {
     const getPovertyData = async () => {
       try {
         
-        const response = await fetch('https://jsonplaceholder.typicode.com/users');  // Replace with your API URL
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');  
         if (!response.ok) {
           throw new Error('Failed to fetch data');
         }
